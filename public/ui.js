b160bc8ad1810163b0905cc5bf1b6a58d7cf9903
emperoroficecream <ebisu1922@gmail.com>
2018-03-03 13:48:33 -0500

    added year selector ui part

--- public/ui.js
+++ public/ui.js
@@ -1,7 +1,24 @@
 const app = new Vue({
   el: '#app',
   data:  {
-    posts: []
+    posts: [],
+    beforeYears: {
+      default: '2012',
+      options: [
+        '2007',
+        '2008',
+        '2009',
+        '2010',
+        '2011',
+        '2012',
+        '2013',
+        '2014',
+        '2015',
+        '2016',
+        '2017',
+        '2018'
+      ]
+    }
   },
   created () {
     fetch('http://localhost:3000/data')
